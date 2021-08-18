a2586bc254d484bf274cb9228778ff2d2f16fc1c
michael-grace <michael.dp.grace@outlook.com>
2021-08-18 13:53:45 +0100

    spaceman replacement schema

---
+++ spaceman-replacement.sql
@@ -0,0 +1,40 @@
+CREATE TABLE hgi_lustre_usage_new.directory(
+    directory_id int AUTO_INCREMENT,
+    project_name text,
+    directory_path text,
+    num_files bigint(8),
+    size bigint(8),
+    last_modified int,
+    
+    pi_id int,
+    volume_id int,
+    group_id int,
+
+    primary key(directory_id),
+
+    foreign key (pi_id) references hgi_lustre_usage_new.pi(pi_id),
+    foreign key (group_id) references hgi_lustre_usage_new.unix_group(group_id),
+    foreign key (volume_id) references hgi_lustre_usage_new.volume(volume_id)
+);
+
+CREATE TABLE hgi_lustre_usage_new.filetype(
+    filetype_id int AUTO_INCREMENT,
+    filetype_name text,
+
+    primary key(filetype_id)
+);
+
+CREATE TABLE hgi_lustre_usage_new.file_count(
+    file_count_id int AUTO_INCREMENT,
+    directory_id int,
+    filetype_id int,
+    total int,
+
+    primary key(file_count_id),
+
+    foreign key (directory_id) references hgi_lustre_usage_new.directory(directory_id),
+    foreign key (filetype_id) references hgi_lustre_usage_new.filetype(filetype_id)
+);
+
+INSERT INTO hgi_lustre_usage_new.filetype (filetype_name)
+    VALUES ("BAM"), ("CRAM"), ("VCF"), ("PEDBED");
