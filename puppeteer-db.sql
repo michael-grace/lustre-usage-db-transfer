7529e619610d8186ccca6d0453876f29b44f9369
michael-grace <michael.dp.grace@outlook.com>
2021-08-31 10:13:16 +0100

    updated puppeteer schema

--- puppeteer-db.sql
+++ puppeteer-db.sql
@@ -6,17 +6,21 @@ CREATE TABLE hgi_lustre_usage_new.vault_actions(
 );
 
 INSERT INTO hgi_lustre_usage_new.vault_actions (action_name)
-    VALUES ("keep"), ("archive");
+    VALUES ('Keep'), ('Archive');
 
 CREATE TABLE hgi_lustre_usage_new.vault(
     record_id int AUTO_INCREMENT,
     record_date date,
     filepath text,
+    group_id int,
     vault_action_id int,
-    ttl int,
-
+    size int,
+    file_owner text,
+    last_modified date,
     volume_id int,
 
     primary key(record_id),
-    foreign key (vault_action_id) references hgi_lustre_usage_new.vault_actions(vault_action_id)
+    foreign key (vault_action_id) references hgi_lustre_usage_new.vault_actions(vault_action_id),
+    foreign key (group_id) references hgi_lustre_usage_new.unix_group(group_id),
+    foreign key (volume_id) references hgi_lustre_usage_new.volume(volume_id)
 );
